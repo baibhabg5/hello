2a5c3105e30028ee91b4575178f779e7ce03e704
baibhabg5 <baibhabg5@gmail.com>
2026-02-11 21:17:39 +0530

    Bookmark Manager with MERN stack and CRUD function

--- bookmarkmanager/server/server.js
+++ bookmarkmanager/server/server.js
@@ -3,13 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Bookmark = require('./models/Bookmark'); // Import the blueprint
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected!"))
@@ -39,6 +37,7 @@ app.post('/api/bookmarks', async (req, res) => {
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
+  
 });
 
 // 3. DELETE a bookmark (The missing piece!)
@@ -51,7 +50,6 @@ app.delete('/api/bookmarks/:id', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
